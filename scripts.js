0ae937e5a2b4d0f956e45192bae36ee2c81b9d76
stannous2 <33492860+stannous2@users.noreply.github.com>
2019-03-29 14:17:36 -0700

    updates

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
 let arr1 = [];
 let arr2 = [];
-let array= [];
+let array = [];
 let cdpStartRow;
 let cdpEndRow;
-let barricadeStartRow
-let barricadeEndRow
+let barricadeStartRow;
+let barricadeEndRow;
+let counter = 0;
 
 function loadASF() {
 
  $('#input-ASF').change(function (e) {
   console.log("Getting data...")
-  if(inputAsfButton.val()){
+  if (inputAsfButton.val()) {
    asfText.html(inputAsfButton.val().match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1])
-  }else {
+  } else {
    asfText.html() = "No file chosen yet..."
   }
 
@@ -33,7 +34,7 @@ function loadASF() {
    // let range = XLSX.utils.decode_range(sheet['!ref']); //get the range
    // let smallerRange = "A2:DE40"
    let range = XLSX.utils.decode_range("A2:A30");
-  
+
    console.log('the whole range... ', range)
 
    for (let R = 1; R <= range.e.r; ++R) {
@@ -50,33 +51,67 @@ function loadASF() {
      console.log('cell_ref: ', cell_ref)
 
      let cell = sheet[cell_ref]
-     debugger
+
      //get the cell ref where its value contains 'CDP 1,2,3,4'
-     if(cell){
-      if (cell.v.includes("CDP")){
+     if (cell) {
+      if (cell.v === ("CDP 1,2,3,4")) {
        console.log('cell ref ', C, R)
-       let cdpStartRow_address = {c: C, r: R+1} // create a new cell_address obj
-       console.log ('cell_address', cdpStartRow_address)
-       cdpStartRow = XLSX.utils.encode_cell(cdpStartRow_address);// create new cell ref for CDP start row
-       
+       let cdpStartRow_address = {
+        c: C,
+        r: R + 1
+       } // create a new cell_address obj
+       console.log('cdpStartRow_address', cdpStartRow_address)
+       cdpStartRow = XLSX.utils.encode_cell(cdpStartRow_address); // create new cell ref for CDP start row
+
        console.log("cdp start row: ", cdpStartRow)
-       
-       debugger
-        if(cell){
-         console.log('cell ref ', C, R)
-        let cdpEndRow_cell_address = {c: C, r: R-1} // create a new cell_address obj
-          console.log ('cell_address', cdpEndRow_cell_address)
-          cdpEndRow = XLSX.utils.encode_cell(cdpEndRow_cell_address);// create new cell ref for CDP start row
-          
-          console.log("cdp end row: ", cdpEndRow)
-        }
+
       }
       
+      
+      if (cell.v === ("Barricade 3,4")) {
+       console.log('cell ref ', C, R)
+       let cdpEndRow_cell_address = {
+        c: C,
+        r: R - 1
+       } // create a new cell_address obj
+       console.log('cdpEndRow_cell_address', cdpEndRow_cell_address)
+       cdpEndRow = XLSX.utils.encode_cell(cdpEndRow_cell_address); // create new cell ref for CDP start row
+
+       console.log("cdp end row: ", cdpEndRow)
+
+       let barricadeStartRow_address = {
+        c: C,
+        r: R + 1
+       } // create a new cell_address obj for barricade
+       console.log('barricadeStartRow_address for ', barricadeStartRow_address)
+       barricadeStartRow = XLSX.utils.encode_cell(barricadeStartRow_address); // create new cell ref for CDP start row
+       console.log("barricade start row: ", barricadeStartRow)
+      }
+
      }
+     debugger
+      if(typeof cell === "undefined"){
+       console.log('cell ref for empty row... ', C, R)
+       
+       if (counter === 1){
+        let cdpEndRow_address = {
+         c: C,
+         r: R - 1
+        } // create a new cell_address obj
+        console.log('cdpEndRow_address ', cdpEndRow_address)
+       } else if(counter === 2){
+        let barricadeEndRow_address = {
+         c: C,
+         r: R - 1
+        } // create a new cell_address obj
+        console.log('barricadeEndRow_address ', barricadeEndRow_address)
+        range.e.r = barricadeEndRow_address.r
+       }
+       counter++
+      }
      }
-     
-    }
-   
+   }
+
    console.log("entire worksheet: ", array)
 
    let desired_range = "E2:CA2" // define desired range
@@ -98,7 +133,7 @@ function loadASF() {
 
      let cell = sheet[cell_ref]
      arr1.push(cell.v)
-     
+
     }
    }
    console.log("array 1: ", arr1)
@@ -110,9 +145,9 @@ function loadArrestmentFile() {
 
  $('#input-arrestLog').change(function (e) {
   console.log("Getting data...")
-  if(inputLogButton.val()){
+  if (inputLogButton.val()) {
    arrestlogText.html(inputLogButton.val().match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1])
-  }else {
+  } else {
    arrestlogText.html() = "No file chosen yet..."
   }
 
@@ -130,7 +165,7 @@ function loadArrestmentFile() {
    let sheet = wb.Sheets[wb.SheetNames[0]]; //get the first worksheet
 
    /* loop through every cell in the worksheet manually */
-  //  let range = XLSX.utils.decode_range(sheet['!ref']); //get the range
+   //  let range = XLSX.utils.decode_range(sheet['!ref']); //get the range
 
    let desired_range = "E2:CA2" // define desired range
    let cell_range = XLSX.utils.decode_range(desired_range) // get the desired range only
